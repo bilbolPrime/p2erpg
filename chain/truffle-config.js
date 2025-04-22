4fde1551752f0c62e4ca1af6482237809079fd0f
Moussa Khalil <moussakhalil1990@gmail.com>
2025-04-22 18:44:42 +0300

    initial chain

---
+++ chain/truffle-config.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+
+function hdWalletProviderOptions(privateKeyEnvVarValue, mnemonicPhraseEnvVarValue, otherOpts) {
+    const opts = {
+        ...otherOpts
+    };
+    if (privateKeyEnvVarValue) {
+        opts.privateKeys = [privateKeyEnvVarValue];
+    } else {
+        opts.mnemonic = mnemonicPhraseEnvVarValue;
+    }
+    return opts;
+}
+
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+module.exports = {
+    networks: {
+        //
+        development: {
+            host: "127.0.0.1", // Localhost (default: none)
+            port: 7545, // Standard Ethereum port (default: none)
+            network_id: "*", // Any network (default: none)
+	}},  plugins: [
+    'truffle-flatten'
+  ]
+};
